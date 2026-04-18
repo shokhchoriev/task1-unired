a6a8443a20c8a43172ebd619b0417fab61a48ae1
turgunov abdurahmon <abdurahmonturgunov212@gmail.com>
2026-04-18 15:55:37 +0500

    add: rgister.admin.py

--- cards/admin.py
+++ cards/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from .models import Card
 
 
-
+@admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     list_display = (
         'formatted_card_number',
@@ -11,9 +12,19 @@ class CardAdmin(admin.ModelAdmin):
         'balance'
     )
 
+    list_filter = (
+        'status',
+        'expire',
+        'balance',
+    )
+
+    search_fields = (
+        'card_number',
+        'phone',
+    )
+
     def formatted_card_number(self, obj):
         return obj.formatted_card_number()
 
     def formatted_phone(self, obj):
         return obj.formatted_phone()
-    
