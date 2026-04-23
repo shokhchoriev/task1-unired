3efc28e0763ca6293ab4eeccbe5ca9411d29362c
MusulmanM <mmmmmmvvv200@gmail.com>
2026-04-23 16:57:09 +0500

    hato tog'irlandi

--- task2/views.py
+++ task2/views.py
@@ -128,3 +128,16 @@ def transfer_history(card_number=None, status=None, date_from=None, date_to=None
     transfers = Transfer.objects.filter(**filters)
     data = [{"ext_id": t.ext_id, "amount": str(t.sending_amount), "state": t.state, "date": t.created_at.isoformat()} for t in transfers]
     return Success(data)
+
+
+
+
+@csrf_exempt
+def json_rpc_view(request):
+    request_data = request.body.decode("utf-8")
+    logger.info(f"Request: {request_data}")
+    
+    response = dispatch(request_data)
+    
+    logger.info(f"Response: {response}")
+    return JsonResponse(response, safe=False)
